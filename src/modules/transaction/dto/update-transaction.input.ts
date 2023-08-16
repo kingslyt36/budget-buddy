00f1f7b2d6d72681f0ddc21efc147292eba79f7d
Duc Quan Nguyen <quanguyen.work@gmail.com>
2023-08-16 20:58:34 +0700

    refactor: Update transaction structure for Graph

---
+++ src/modules/transaction/dto/update-transaction.input.ts
@@ -0,0 +1,6 @@
+import { CreateTransactionInput } from './create-transaction.input';
+import { PartialType } from '@nestjs/mapped-types';
+
+export class UpdateTransactionInput extends PartialType(CreateTransactionInput) {
+    id: number;
+}
---
+++ src/modules/transaction/transaction.resolver.ts
@@ -0,0 +1,34 @@
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { TransactionService } from './transaction.service';
+import { CreateTransactionInput } from './dto/create-transaction.input';
+import { UpdateTransactionInput } from './dto/update-transaction.input';
+
+@Resolver('Transaction')
+export class TransactionResolver {
+    constructor(private readonly transactionService: TransactionService) {}
+
+    @Mutation('createTransaction')
+    create(@Args('createTransactionInput') createTransactionInput: CreateTransactionInput) {
+        return this.transactionService.create(createTransactionInput);
+    }
+
+    @Query('transaction')
+    findAll() {
+        return this.transactionService.findAll();
+    }
+
+    @Query('transaction')
+    findOne(@Args('id') id: number) {
+        return this.transactionService.findOne(id);
+    }
+
+    @Mutation('updateTransaction')
+    update(@Args('updateTransactionInput') updateTransactionInput: UpdateTransactionInput) {
+        return this.transactionService.update(updateTransactionInput.id, updateTransactionInput);
+    }
+
+    @Mutation('removeTransaction')
+    remove(@Args('id') id: number) {
+        return this.transactionService.remove(id);
+    }
+}

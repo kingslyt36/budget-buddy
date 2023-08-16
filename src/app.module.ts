e7c62915634eaef7080bfa1dc302a6f66d22aac9
Duc Quan Nguyen <quanguyen.work@gmail.com>
2023-08-16 20:52:27 +0700

    feat: Config GraphQL for this project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,34 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { join } from 'path';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
-import { ConfigModule } from '@nestjs/config';
-
+import { DatabaseModule } from './modules/database/database.module';
+import { PortfolioModule } from './portfolio/portfolio.module';
+import { TransactionModule } from './transaction/transaction.module';
 @Module({
-    imports: [UserModule, ConfigModule.forRoot({ isGlobal: true })],
+    imports: [
+        UserModule,
+        ConfigModule.forRoot({ isGlobal: true }),
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+            driver: ApolloDriver,
+            playground: false,
+            plugins: [ApolloServerPluginLandingPageLocalDefault()],
+            typePaths: ['./**/*.graphql'],
+            definitions: {
+                path: join(process.cwd(), 'src/types/graphql.ts'),
+                outputAs: 'class',
+            },
+        }),
+        DatabaseModule,
+        PortfolioModule,
+        TransactionModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })

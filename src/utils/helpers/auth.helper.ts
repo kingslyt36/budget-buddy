d5cffb0a73d6b72dedaee554123db275e9e2779f
Duc Quan Nguyen <quanguyen.work@gmail.com>
2023-08-25 12:08:42 +0700

    chore: Move instants and helpers to utils folder

--- src/utils/helpers/auth.helper.ts
+++ src/utils/helpers/auth.helper.ts
@@ -1,6 +1,6 @@
 import * as argon2 from 'argon2';
 
-export async function hashPassword(password: string): Promise<string> {
+export async function hashData(password: string): Promise<string> {
     return argon2.hash(password);
 }
 
